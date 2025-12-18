feaa38efaa814c4560d3292540cceb1c43a2c738
HuyHoangFSDev <nguyenthehuyhoang2002@gmail.com>
2025-12-18 14:25:18 +0700

    fix(all): remove unused state and fix imports in HeroSlider component

--- vite.config.js
+++ vite.config.js
@@ -5,56 +5,56 @@ import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 import { createHtmlPlugin } from 'vite-plugin-html';
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-    ViteImageOptimizer({
-      webp: { quality: 80 },
-      jpg: { quality: 80 },
-      png: { quality: 80 },
-    }),
-    createHtmlPlugin({
-      minify: true,
-      inject: {
-        tags: [
-          {
-            tag: 'link',
-            attrs: {
-              rel: 'preload',
-              as: 'style',
-              href: '/assets/index.css',
+    plugins: [
+        react(),
+        tailwindcss(),
+        ViteImageOptimizer({
+            webp: { quality: 80 },
+            jpg: { quality: 80 },
+            png: { quality: 80 },
+        }),
+        createHtmlPlugin({
+            minify: true,
+            inject: {
+                tags: [
+                    {
+                        tag: 'link',
+                        attrs: {
+                            rel: 'preload',
+                            as: 'style',
+                            href: '/assets/index.css',
+                        },
+                        injectTo: 'head',
+                    },
+                    {
+                        tag: 'script',
+                        attrs: {
+                            src: '/src/main.tsx',
+                            type: 'module',
+                            defer: true,
+                        },
+                        injectTo: 'body',
+                    },
+                ],
             },
-            injectTo: 'head',
-          },
-          {
-            tag: 'script',
-            attrs: {
-              src: '/src/main.tsx',
-              type: 'module',
-              defer: true,
-            },
-            injectTo: 'body',
-          },
-        ],
-      },
-    }),
-  ],
-  resolve: {
-    alias: {
-      '@': '/src',
-      '@components': '/src/components',
-      '@pages': '/src/pages',
-      '@hooks': '/src/hooks',
-      '@services': '/src/services',
-      '@utils': '/src/utils',
-      '@constants': '/src/constants',
-      '@app-types': '/src/types',
-      '@stores': '/src/stores',
-      '@assets': '/src/assets',
-      '@styles': '/src/styles',
-      '@routes': '/src/routes',
-      '@base': '/src/base',
-      '@features': '/src/features',
+        }),
+    ],
+    resolve: {
+        alias: {
+            '@': '/src',
+            '@components': '/src/components',
+            '@pages': '/src/pages',
+            '@hooks': '/src/hooks',
+            '@services': '/src/services',
+            '@utils': '/src/utils',
+            '@constants': '/src/constants',
+            '@app-types': '/src/types',
+            '@stores': '/src/stores',
+            '@assets': '/src/assets',
+            '@styles': '/src/styles',
+            '@routes': '/src/routes',
+            '@base': '/src/base',
+            '@features': '/src/features',
+        },
     },
-  },
 });
